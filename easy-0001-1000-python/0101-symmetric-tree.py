a70f1de5f9d46bc49e7f081e9faeb3259cab1a98
Beatrice <tanyuqing10@gmail.com>
2025-09-16 19:47:03 -0400

    Update

---
+++ easy-0001-1000-python/0101-symmetric-tree.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/symmetric-tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if root == None or (root.left == None and root.right == None):
+            return True
+        elif root.left != None and root.right != None and root.left.val == root.right.val and \
+            self.isSymmetric(TreeNode(0, root.left.left, root.right.right)) and \
+            self.isSymmetric(TreeNode(0, root.left.right, root.right.left)):
+            return True
+        else:
+            return False
+
---
+++ easy-0001-1000-python/0108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 0:
+            return None
+        else:
+            mid = len(nums) / 2
+            return TreeNode(nums[mid], self.sortedArrayToBST(nums[:mid]), self.sortedArrayToBST(nums[mid+1:]))
+
