c5b0a647b6af2ed4915e2517104abd9b340bac14
Beatrice <tanyuqing10@gmail.com>
2025-10-20 14:53:01 -0400

    Update

--- utility/leetcode_touch.sh
+++ utility/leetcode_touch.sh
@@ -8,14 +8,18 @@ ext_default=py
 [ -z ${start} ] && exit
 [ -z ${end} ] && exit
 [ ${start} -gt ${end} ] && exit
+dir_default=$(printf "%04d-%04d" $(( ${start} / 1000 * 1000 + 1 )) $(( ${end} / 1000 * 1000 )))
 
 for i in {${start}..${end}}; do
-    problem=$(curl "https://leetcode-api-pied.vercel.app/problem/${i}" 2>/dev/null | jq "del(.content) | del(.hints) | del(.solution)")
-    categoryTitle=$(echo ${problem} | jq -r ".categoryTitle")
-    difficulty=$(echo ${problem} | jq -r ".difficulty" | tr '[:upper:]' '[:lower:]')
-    isPaidOnly=$(echo ${problem} | jq -r ".isPaidOnly")
-    url=$(echo ${problem} | jq -r ".url" | sed "s|/$||g")
-    url_file=$(echo ${url} | sed "s|.*/||g")
+    difficulty=""
+    while [[ ${difficulty} == "" ]]; do
+        problem=$(curl "https://leetcode-api-pied.vercel.app/problem/${i}" 2>/dev/null | jq "del(.content) | del(.hints) | del(.solution)")
+        categoryTitle=$(echo ${problem} | jq -r ".categoryTitle")
+        difficulty=$(echo ${problem} | jq -r ".difficulty" | tr '[:upper:]' '[:lower:]')
+        isPaidOnly=$(echo ${problem} | jq -r ".isPaidOnly")
+        url=$(echo ${problem} | jq -r ".url" | sed "s|/$||g")
+        url_file=$(echo ${url} | sed "s|.*/||g")
+    done
 
     if [[ ${categoryTitle} == "Algorithms" ]] || [[ ${categoryTitle} == "Concurrency" ]]; then
         lang=${lang_default}
@@ -24,7 +28,7 @@ for i in {${start}..${end}}; do
         lang=sql
         ext=sql
     elif [[ ${categoryTitle} == "JavaScript" ]]; then
-        lang=js
+        lang=javascript
         ext=js
     elif [[ ${categoryTitle} == "Shell" ]]; then
         lang=shell
@@ -38,7 +42,7 @@ for i in {${start}..${end}}; do
     else
         isPaidOnly=""
     fi
-    dir=$(printf "%04d-%04d${isPaidOnly}-${difficulty}-${lang}" ${start} ${end} | t)
+    dir=$(printf "${dir_default}${isPaidOnly}-${difficulty}-${lang}" | t)
     printf "/"
     file=$(printf "%04d-%s\n" ${i} ${url_file} | t)
 
