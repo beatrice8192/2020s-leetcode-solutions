988c01b7b5ccdcc8cb73b292829f4d239706367c
Beatrice <tanyuqing10@gmail.com>
2025-10-29 15:00:40 -0400

    Update

--- 3001-4000-easy-python/3370-smallest-number-with-all-set-bits.py
+++ 3001-4000-easy-python/3370-smallest-number-with-all-set-bits.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/smallest-number-with-all-set-bits
+class Solution(object):
+    # def smallestNumber(self, n: int) -> int:
+    def smallestNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return (1 << n.bit_length()) - 1
+
