dd9bd0f5f0edca3d4d85b651aac7001bc3db9e05
Beatrice <tanyuqing10@gmail.com>
2025-11-10 14:53:24 -0500

    Update

--- 0001-1000-easy-python/0746-min-cost-climbing-stairs.py
+++ 0001-1000-easy-python/0746-min-cost-climbing-stairs.py
@@ -1,1 +1,13 @@
 # https://leetcode.com/problems/min-cost-climbing-stairs
+class Solution(object):
+    # def minCostClimbingStairs(self, cost: List[int]) -> int:
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp_result = [0] * len(cost)
+        for i in range(2, len(cost)):
+            dp_result[i] = min(dp_result[i-1] + cost[i-1], dp_result[i-2] + cost[i-2])
+        return min(dp_result[-1] + cost[-1], dp_result[-2] + cost[-2])
+
--- 0001-1000-easy-python/0747-largest-number-at-least-twice-of-others.py
+++ 0001-1000-easy-python/0747-largest-number-at-least-twice-of-others.py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/largest-number-at-least-twice-of-others
+class Solution(object):
+    # def dominantIndex(self, nums: List[int]) -> int:
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_sorted = sorted((nums[i], i) for i in range(len(nums)))
+        return nums_sorted[-1][1] if nums_sorted[-1][0] >= nums_sorted[-2][0] * 2 else -1
+
