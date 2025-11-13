5bfd3ce18c8214fd3c71bcd9c04a12caf201f4f5
Beatrice <tanyuqing10@gmail.com>
2025-11-12 22:35:49 -0500

    Update

--- 0001-1000-easy-python/0804-unique-morse-code-words.py
+++ 0001-1000-easy-python/0804-unique-morse-code-words.py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/unique-morse-code-words
+class Solution(object):
+    # def uniqueMorseRepresentations(self, words: List[str]) -> int:
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        return len(set(["".join([morse[ord(char) - 97] for char in word]) for word in words]))
+
--- 0001-1000-easy-python/0806-number-of-lines-to-write-string.py
+++ 0001-1000-easy-python/0806-number-of-lines-to-write-string.py
@@ -1,1 +1,20 @@
 # https://leetcode.com/problems/number-of-lines-to-write-string
+class Solution(object):
+    # def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+    def numberOfLines(self, widths, s):
+        """
+        :type widths: List[int]
+        :type s: str
+        :rtype: List[int]
+        """
+        lines = 1
+        length = 0
+        def getWidth(char):
+            return widths[ord(char) - 97]
+        for char in s:
+            length += getWidth(char)
+            if (length > 100):
+                lines += 1
+                length = getWidth(char)
+        return [lines, length]
+
